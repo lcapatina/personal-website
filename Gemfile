d465e0b3bb5310ab7afad0c1c5cab0f83fd70a6e
Laurentiu Capatina <lcapatina@mantu.com>
2020-04-01 17:13:25 +0200

    started trying a one page scrolling version

--- Gemfile
+++ Gemfile
@@ -29,3 +29,5 @@ end
 gem "wdm", "~> 0.1.1", :install_if => Gem.win_platform?
 
 gem 'milligram', '~> 1.3'
+gem "therubyracer"
+gem 'jekyll-coffeescript'
---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+document.getElementById("home-button").onclick=function(){
+  console.log("button clicked!");
+  document.getElementById('home').scrollIntoView();
+}
+
+document.getElementById("aboutme-button").onclick=function(){
+  console.log("button clicked!");
+  document.getElementById('aboutme').scrollIntoView();
+}

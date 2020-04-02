4b4317ed87a95c39df6ea6b18edc6c80f3b61c4a
Laurentiu Capatina <lcapatina@mantu.com>
2020-04-02 17:22:37 +0200

    decent version of 2 section movement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 document.getElementById("home-button").onclick=function(){
   console.log("button clicked!");
+  //document.getElementById('aboutme').classList.remove("selected-section");
+  //document.getElementById('home').classList.add("selected-section");
   document.getElementById('home').scrollIntoView();
 }
 
 document.getElementById("aboutme-button").onclick=function(){
   console.log("button clicked!");
+  //document.getElementById('home').classList.remove("selected-section");
+  //document.getElementById('aboutme').classList.add("selected-section");
   document.getElementById('aboutme').scrollIntoView();
 }

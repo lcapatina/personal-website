cd428f0281aae53791214873781a563248c02638
Laurentiu Capatina <lcapatina@mantu.com>
2020-04-03 14:57:29 +0200

    stable version for home, aboutme and education

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,7 @@
-document.getElementById("home-button").onclick=function(){
-  console.log("button clicked!");
-  //document.getElementById('aboutme').classList.remove("selected-section");
-  //document.getElementById('home').classList.add("selected-section");
-  document.getElementById('home').scrollIntoView();
-}
-
-document.getElementById("aboutme-button").onclick=function(){
-  console.log("button clicked!");
-  //document.getElementById('home').classList.remove("selected-section");
-  //document.getElementById('aboutme').classList.add("selected-section");
-  document.getElementById('aboutme').scrollIntoView();
-}
+Array.from(document.getElementById('cv-menu').children).forEach(element => {
+  var buttonId = element.children[0].id;
+  var sectionId = buttonId.split("-")[0];
+  document.getElementById(buttonId).onclick=function(){
+    document.getElementById(sectionId).scrollIntoView();
+  }
+});
